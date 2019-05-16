98fe32b3f6b944638460b8e658c573cc1929b2de
Andrey <avedishchev@list.ru>
2019-05-17 01:16:04 +0300

    1.3. JavaDoc. Code convention[#141918]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,12 @@
 package ru.job4j.calculate;
 
+/**
+ * Class Класс для вычисления арифметических операций + - / *
+ * @autor Andrey Vedishchev
+ * @sience 17.05.2019
+ * @version 1
+ */
+
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Andrey Vedishchev (avedishchev@list.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
