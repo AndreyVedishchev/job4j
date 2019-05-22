c9afd91fc23924971c5fce19e1f891062185a2bb
Andrey <avedishchev@list.ru>
2019-05-22 22:38:31 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#141885]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+/**
+ * Суммирует числа в цикле
+ */
+public class Counter {
+
+    /**
+     * add
+     * @param start - стартовое число
+     * @param finish - конечное число
+     * @return сумма четных чисел в диапазоне
+     */
+    public int add(int start, int finish) {
+        int sum = 0;
+
+        for(int i = start; i <= finish; i++) {
+            if(i % 2 == 0) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
