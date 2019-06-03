1e4d1808f7f4a6f2386c7fe9b08ffdca61013ada
Andrey <avedishchev@list.ru>
2019-06-03 21:50:50 +0300

    6.8. Удаление дубликатов в массиве.[#141899] решение за 2 цикла

--- chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayDuplicateTest.java
@@ -2,23 +2,25 @@ package ru.job4j.array;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
 import static org.junit.Assert.assertThat;
 
 public class ArrayDuplicateTest {
     @Test
     public void whenRemoveDuplicatesThenArrayWithoutDuplicate() {
-        String[] arr = {"Привет", "Мир", "Привет", "Супер", "Мир"};
-        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
-        String[] result = arrayDuplicate.remove(arr);
-        assertThat(result, is(new String[]{"Привет", "Мир", "Супер"}));
+        String[] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
+        String[] expect = {"Привет", "Мир", "Супер"};
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] result = duplicate.remove(input);
+        assertThat(result, arrayContainingInAnyOrder(expect));
     }
 
     @Test
     public void whenRemoveDuplicatesThenArrayWithoutDuplicate2() {
-        String[] arr = {"Привет", "Привет", "Привет", "Привет", "Привет", "Привет"};
-        ArrayDuplicate arrayDuplicate = new ArrayDuplicate();
-        String[] result = arrayDuplicate.remove(arr);
-        assertThat(result, is(new String[]{"Привет"}));
+        String[] input = {"Привет", "Привет", "Привет", "Привет", "Привет", "Привет"};
+        String[] expect = {"Привет"};
+        ArrayDuplicate duplicate = new ArrayDuplicate();
+        String[] result = duplicate.remove(input);
+        assertThat(result, arrayContainingInAnyOrder(expect));
     }
 }
