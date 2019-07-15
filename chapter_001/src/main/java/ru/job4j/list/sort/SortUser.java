1b3ca6a718bc8318c14573f8b87daed4562383d8
Andrey <avedishchev@list.ru>
2019-07-15 14:36:21 +0300

    2. Сортировка User с использованием Comparator[#141935]

--- chapter_001/src/main/java/ru/job4j/list/sort/SortUser.java
+++ chapter_001/src/main/java/ru/job4j/list/sort/SortUser.java
@@ -8,12 +8,44 @@ public class SortUser {
     }
 
     public List<User> sortNameLength (List<User> list) {
-        //Collections.sort();
-        return null;
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        return o1.getNameSize().compareTo(o2.getNameSize());
+                    }
+                }
+        );
+        return list;
     }
 
     public List<User> sortByAllFields (List<User> list) {
-        //Collections.sort();
-        return null;
+        list.sort(
+                new Comparator<User>() {
+                    @Override
+                    public int compare(User o1, User o2) {
+                        if (o1.getName().compareTo(o2.getName()) != 0) {
+                            return o1.getName().compareTo(o2.getName());
+                        } else {
+                            return o1.getAge().compareTo(o2.getAge());
+                        }
+                    }
+                }
+        );
+        return list;
+    }
+
+    public static void main(String[] args) {
+        SortUser sortUser = new SortUser();
+        List<User> list = new ArrayList<>(Arrays.asList(
+                new User("25", "Сергей"),
+                new User("30", "Сергей"),
+                new User("25", "Иван"),
+                new User("30", "Иван")
+        ));
+
+        System.out.println(list);
+        System.out.println(sortUser.sortByAllFields(list));
+        System.out.println(sortUser.sortNameLength(list));
     }
 }
--- chapter_001/src/main/java/ru/job4j/list/sort/User.java
+++ chapter_001/src/main/java/ru/job4j/list/sort/User.java
@@ -3,10 +3,24 @@ package ru.job4j.list.sort;
 public class User implements Comparable<User> {
     private String age;
     private String name;
+    private String nameSize;
 
     public User(String age, String name) {
         this.age = age;
         this.name = name;
+        this.nameSize = String.valueOf(name.length());
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getNameSize() {
+        return nameSize;
     }
 
     @Override
