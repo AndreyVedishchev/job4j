ed21bc0a766ddf598aed27a4e935be5a1b234f3f
Andrey <avedishchev@list.ru>
2019-07-20 23:57:58 +0300

    4. Преобразование List в Map.[#141840]

--- chapter_002/src/main/java/ru/job4j/tracker/student/Student.java
+++ chapter_002/src/main/java/ru/job4j/tracker/student/Student.java
@@ -2,16 +2,20 @@ package ru.job4j.tracker.student;
 
 public class Student {
     public int score;
+    public String surname;
 
-    public Student(int score) {
+    public Student(int score, String surname) {
         this.score = score;
+        this.surname = surname;
     }
 
     @Override
     public String toString() {
-        return "Student{"
-                + "score="
+        return  "score="
                 + score
+                + ", surname='"
+                + surname
+                + '\''
                 + '}';
     }
 }
--- chapter_002/src/test/java/ru/job4j/tracker/student/SchoolTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/student/SchoolTest.java
@@ -2,9 +2,7 @@ package ru.job4j.tracker.student;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -18,22 +16,23 @@ public class SchoolTest {
         School school = new School();
         List<Student> list = new ArrayList<>(
                 Arrays.asList(
-                        new Student(10),
-                        new Student(55),
-                        new Student(80),
-                        new Student(64),
-                        new Student(96),
-                        new Student(42)
+                        new Student(10, "s1"),
+                        new Student(55, "s2"),
+                        new Student(80, "s3"),
+                        new Student(64, "s4"),
+                        new Student(96, "s5"),
+                        new Student(42, "s6")
                 )
         );
         Predicate<Student> predicate = s -> s.score >= 70;
-        List<Student> res = school.collect(list, predicate);
-        assertThat(res.toString(), is(
-                Arrays.asList(
-                        new Student(80),
-                        new Student(96)
-                ).toString()
-        ));
+        Map<String, Student> res = school.collect(list, predicate);
+        Map<String, Student> exp = new HashMap<>(
+                Map.of(
+                        "s3", new Student(80, "s3"),
+                        "s5", new Student(96, "s5")
+                )
+        );
+        assertThat(res.toString(), is(exp.toString()));
     }
 
     @Test
@@ -41,23 +40,24 @@ public class SchoolTest {
         School school = new School();
         List<Student> list = new ArrayList<>(
                 Arrays.asList(
-                        new Student(10),
-                        new Student(55),
-                        new Student(80),
-                        new Student(64),
-                        new Student(96),
-                        new Student(42)
+                        new Student(10, "s1"),
+                        new Student(55, "s2"),
+                        new Student(80, "s3"),
+                        new Student(64, "s4"),
+                        new Student(96, "s5"),
+                        new Student(42, "s6")
                 )
         );
         Predicate<Student> predicate = s -> s.score > 50
                 && s.score < 70;
-        List<Student> res = school.collect(list, predicate);
-        assertThat(res.toString(), is(
-                Arrays.asList(
-                        new Student(55),
-                        new Student(64)
-                ).toString()
-        ));
+        Map<String, Student> res = school.collect(list, predicate);
+        Map<String, Student> exp = new HashMap<>(
+                Map.of(
+                        "s2", new Student(55, "s2"),
+                        "s4", new Student(64, "s4")
+                )
+        );
+        assertThat(res.toString(), is(exp.toString()));
     }
 
     @Test
@@ -65,21 +65,21 @@ public class SchoolTest {
         School school = new School();
         List<Student> list = new ArrayList<>(
                 Arrays.asList(
-                        new Student(10),
-                        new Student(55),
-                        new Student(80),
-                        new Student(64),
-                        new Student(96),
-                        new Student(42)
+                        new Student(10, "s1"),
+                        new Student(55, "s2"),
+                        new Student(80, "s3"),
+                        new Student(64, "s4"),
+                        new Student(96, "s5"),
+                        new Student(42, "s6")
                 )
         );
         Predicate<Student> predicate = s -> s.score < 50;
-        List<Student> res = school.collect(list, predicate);
+        Map<String, Student> res = school.collect(list, predicate);
         assertThat(res.toString(), is(
-                Arrays.asList(
-                        new Student(10),
-                        new Student(42)
-                ).toString()
-        ));
+                Map.of(
+                        "s1", new Student(10, "s1"),
+                        "s6", new Student(42, "s6")
+                ).toString())
+        );
     }
 }
