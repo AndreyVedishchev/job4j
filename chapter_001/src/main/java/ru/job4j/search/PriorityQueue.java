8a48f92b9c230953fb966330d6dc11336702a122
Andrey <avedishchev@list.ru>
2019-07-23 01:23:36 +0300

    2. Local-Variable Type Inference[#141845]

--- chapter_001/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_001/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -12,8 +12,8 @@ public class PriorityQueue {
      * @param task задача
      */
     public void put(Task task) {
-        int count = tasks.size();
-        for (int i = 0; i < tasks.size(); i++) {
+        var count = tasks.size();
+        for (var i = 0; i < tasks.size(); i++) {
             if (task.getPriority() < tasks.get(i).getPriority()) {
                 count = i;
                 break;
